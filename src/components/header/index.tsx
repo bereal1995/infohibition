7c652f2368210d7d1d216e2f2cf085c8f2093d69
bereal1995 <gygud98@gmail.com>
2023-01-12 17:20:15 +0900

    feat: svgr 추가

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import Logo from '@/img/logo.svg';
+
+function Header() {
+  return (
+    <header>
+      <h1>My App</h1>
+      <Logo />
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { Noto_Sans_KR } from '@next/font/google';
 
 import '@/styles/globals.css';
+import Header from '@/src/components/header';
 
 const notoSansKr = Noto_Sans_KR({
   weight: '400',
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
           }
         `}
       </style>
+      <Header />
       <Component {...pageProps} />
     </>
   );
