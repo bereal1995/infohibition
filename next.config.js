3b24ea7e36a85be7c216a74163b407b3eb52f776
bereal1995 <gygud98@gmail.com>
2023-01-12 17:07:13 +0900

    feat: tailwindcss 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  fontLoaders: [
-    { loader: '@next/font/google', options: { subsets: ['latin'] } },
-  ],
 };
 
 module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import { Noto_Sans_KR } from '@next/font/google';
 
+import '@/styles/globals.css';
+
 const notoSansKr = Noto_Sans_KR({
   weight: '400',
   subsets: ['latin'],
