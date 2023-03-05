5d73e7578b45b5623acc0a62cc4786a3cd3fadab
bereal1995 <gygud98@gmail.com>
2023-03-05 13:07:45 +0900

    feat: 상세페이지 ssr제거

--- src/pages/items/[seq].tsx
+++ src/pages/items/[seq].tsx
@@ -1,9 +1,5 @@
-import { GetServerSideProps } from 'next';
-import { dehydrate, QueryClient } from '@tanstack/react-query';
-
 import DetailContainer from '@/components-pages/detail';
 import DetailLayout from '@/components-shared/layouts/DetailLayout';
-import { getItem } from '@/api/items';
 import ThemeButton from '@/components-shared/buttons/ThemeButton';
 
 export default function DetailPage() {
@@ -13,16 +9,3 @@ export default function DetailPage() {
     </DetailLayout>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const queryClient = new QueryClient();
-
-  const { seq } = ctx.query;
-  await queryClient.prefetchQuery(['item', seq], () => getItem(seq as string));
-
-  return {
-    props: {
-      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-    },
-  };
-};
